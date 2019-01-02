45c4ea9d65b4854db3e3a7a0654bd7ac7d8047ad
Chris Honda <christopher.honda92@gmail.com>
2019-01-02 15:40:49 -0800

    Fixed the footer to the bottom of the page; smoothed out footer content styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,33 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => {
+    const Wrapper = styled.div`
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    `
     const About = styled.div`
         text-align: center;
     `;
     const Credits = styled.div`
         text-align: center;
         background-color: lightgray;
+        h4 {
+            padding-top: 10px;
+        }
     `;
     const List = styled.ul`
-        column-count: 3;
-        position: center;
+        /* column-count: 3;
+        column-width: 300px; */
+        display: flex;
+        justify-content: center;
+        list-style-type: none;
+    `
+    const Items = styled.li`
+        padding: 0 20px 20px 20px;
     `
     return (
         <div>
-            <About>
-                <h2>About this Calculator</h2>
-                <p>This is a lazy person's Lorem Ipsum. This is a lazy person's Lorem Ipsum. This is a lazy person's Lorem Ipsum.</p>
-            </About>
-            <Credits>
-                <h4>Created by:</h4>
-                <List>
-                    <li>Jeanette Fernandez</li>
-                    <li>Leigh-Ann Friedel</li>
-                    <li>Colin Dismuke</li>
-                    <li>Tommy Huynh</li>
-                    <li>Chris Honda</li>
-                </List>
-            </Credits>
+            <Wrapper>
+                <About>
+                    <h2>About this Calculator</h2>
+                    <p>Text here</p>
+                </About>
+                <Credits>
+                    <h4>Created by:</h4>
+                    <List>
+                        <Items>Jeanette Fernandez</Items>
+                        <Items>Leigh-Ann Friedel</Items>
+                        <Items>CoItemsn Dismuke</Items>
+                        <Items>Tommy Huynh</Items>
+                        <Items>Chris Honda</Items>
+                    </List>
+                </Credits>
+            </Wrapper>
             </div>
     );
 };
