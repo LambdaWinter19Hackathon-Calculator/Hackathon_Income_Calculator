acf0217327d35719437e6d6efca7ee122bee612d
Jeanette <jeanfern555@gmail.com>
2019-01-02 18:57:34 -0600

    Added styled components because graph was getting cut off.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-// import IncomeForm from "./components/IncomeForm";
-import Graph from "./components/Graph";
-import '../node_modules/react-vis/dist/style.css';
-=======
 import Header from './components/Header';
 import Footer from './components/Footer';
 import IncomeForm from "./components/IncomeForm";
->>>>>>> 0b31618d190809a18741d86370743c867dc9b26f
+import Graph from "./components/Graph";
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        {/* <IncomeForm /> */}
-        <Graph />
-=======
       <div>
         <Header />
-          <IncomeForm />
+          <ContentContainer>
+            <IncomeForm />
+            <Graph />
+          </ContentContainer>
         <Footer />
->>>>>>> 0b31618d190809a18741d86370743c867dc9b26f
       </div>
     );
   }
 }
+
+const ContentContainer = styled.div`
+    display: flex;
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import '../node_modules/react-vis/dist/style.css';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
