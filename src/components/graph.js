380ad19dcf97f53b73642c116d53bfc27a3263b3
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-02 23:59:58 -0600

    Adjusted margin of chart to show full earnings number

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import {
-  XYPlot,
   FlexibleXYPlot,
   XAxis,
   YAxis,
@@ -15,7 +14,7 @@ const GraphContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   width: 100%;
-  padding: 0px 20px;
+  padding-right: 20px;
 `;
 
 class Graph extends Component {
@@ -40,21 +39,30 @@ class Graph extends Component {
 
     return (
       <GraphContainer>
-        <FlexibleXYPlot>
-          <XAxis title="Year" />
-          <YAxis title="" />
-          <HorizontalGridLines />
-          <VerticalGridLines />
+        <FlexibleXYPlot margin={{ left: 100 }}>
+          <XAxis title="Year" style={{ fontSize: 14 }} />
+          <YAxis title="Total Earnings" style={{ fontSize: 14 }} />
+          <HorizontalGridLines style={{ stroke: "hsl(42, 15%, 90%)" }} />
+          <VerticalGridLines style={{ stroke: "hsl(42, 15%, 90%)" }} />
 
           <LineSeries
             className="preLambda"
             data={annualEarnings(50000, 0.03, 40)}
+            style={{
+              strokeLineJoin: "round",
+              strokeWidth: 4,
+              stroke: "hsl(36, 77%, 75%)"
+            }}
           />
           <LineSeries
             className="postLambda"
             curve={"curveMonotoneX"}
             data={annualEarnings(100000, 0.03, 40)}
-            strokeDasharray={"7, 3"}
+            style={{
+              strokeLineJoin: "round",
+              strokeWidth: 4,
+              stroke: "hsl(0, 92%, 20%)"
+            }}
           />
         </FlexibleXYPlot>
       </GraphContainer>
