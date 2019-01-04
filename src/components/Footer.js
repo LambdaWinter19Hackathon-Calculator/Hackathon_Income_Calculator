256e5762e15fb7b25d82cc557def73f88f9280a7
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-03 20:41:10 -0500

    Added About lorem ipsum, responsive styles complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Wrapper>
                 <About>
                     <h2>About this Calculator</h2>
-                    <p>Text here</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 </About>
                 <Credits>
                     <h4>Created by</h4>
@@ -31,11 +31,25 @@ const Footer = () => {
 const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
-padding-top: 60px;
+padding-top: 20px;
 `
 const About = styled.div`
 text-align: center;
 padding-bottom: 30px;
+width: 50%;
+margin: 0 auto;
+
+@media (max-width: 1024px) {
+    width: 65%;
+}
+
+@media (max-width: 650px) {
+    width: 85%;
+}
+
+h2 {
+    font-size: 1.4rem;
+}
 `;
 
 const Credits = styled.div`
@@ -43,6 +57,7 @@ text-align: center;
 background-color: lightgray;
 h4 {
     padding: 20px;
+    font-size: 1.1rem;
 }
 `;
 const List = styled.ul`
