29084b0e67f588a5afaa6918bfe333d6ed024936
Miikis <28997062+Miikis@users.noreply.github.com>
2019-03-27 12:37:18 -0700

    Extract vendor styles into separate scss file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+import "./scss/index.scss"
+
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/react-vis/dist/style.css"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware, compose } from "redux"
 import rootReducer from "./store/reducers/rootReducer"
