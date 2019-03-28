aa2368595b343e60a22d1eefe7bcae4cd2a5d0c6
Miikis <28997062+Miikis@users.noreply.github.com>
2019-03-27 18:43:12 -0700

    Extract common Sass variables into separate file

--- src/components/TweetButton/index.js
+++ src/components/TweetButton/index.js
@@ -1,23 +1,8 @@
 import React from "react"
-import styled from "styled-components"
-
-const Tweet = styled.div`
-    display: flex;
-    justify-content: center;
-
-    @media (max-width: 1024px) {
-        align-items: center;
-    }
-
-    @media (max-width: 600px) {
-        width: 100%;
-    }
-`
 
 const TweetButton = props => {
-    // console.log(props.tweetMsg);
     return (
-        <Tweet>
+        <div class="tweet-btn">
             <a
                 className="twitter-share-button"
                 href={`https://twitter.com/intent/tweet`}
@@ -28,7 +13,7 @@ const TweetButton = props => {
             >
                 Tweet
             </a>
-        </Tweet>
+        </div>
     )
 }
 
