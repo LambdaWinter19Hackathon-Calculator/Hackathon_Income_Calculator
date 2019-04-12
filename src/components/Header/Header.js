8396d74a8014b126e9f4c63bdf3c52fa3d9ce68b
ToMMaaY <thuynh629@gmail.com>
2019-04-12 10:58:18 -0700

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../../assets/logo_color.png';
+import logo from '../../assets/lambda-logo.png';
+import { colors } from '../../style/colors';
 
 const Header = () => {
     return (
-        <Title>
-            <img src={logo} alt="Compound logo in Lambda School red" />
-        </Title>
+        <HeaderContainer>
+            <div className="logo-content">
+                <img src={logo} alt="Compound logo in Lambda School red" />
+                <p className="compound">Compound</p>
+            </div>
+        </HeaderContainer>
     );
 };
 
 //CSS ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const Title = styled.div`
+const HeaderContainer = styled.div`
     display: flex;
     justify-content: center;
-    background-color: #e8e6e1;
-    padding: 25px;
-    max-height: 125px;
+    border-bottom: 0.5px solid rgb(235, 235, 235);
+    padding: 20px;
+    max-height: 70px;
+    background: ${colors.headerBg};
 
-    img {
-        width: 300px;
+    .logo-content {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+
+        img {
+            height: 30px;
+            width: auto;
+            margin-right: 7px;
+        }
+
+        .compound {
+            text-transform: uppercase;
+            color: ${colors.secondRed};
+            font-size: 12px;
+            font-weight: 500;
+            padding-top: 5px;
+            letter-spacing: -0.5px;
+        }
     }
 `;
 
---
+++ src/style/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    mainRed: 'rgb(149,4,28)',
+    secondRed: 'rgb(165,15,41)',
+    graphPreLambda: 'rgb(255,6,65)',
+    graphPostLambda: 'rgb(41,160,221)',
+    bodyBg: 'rgb(245,247,249)',
+    headerBg: 'white',
+    footerBg: 'rgb(29,35,38)',
+    font: 'rgb(105,111,115)',
+};
