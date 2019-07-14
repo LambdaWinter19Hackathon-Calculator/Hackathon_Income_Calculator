0eb97e86f2b907e52d8fe474feef322f98abee54
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-14 17:50:43 -0400

    Remove React router

--- src/index.js
+++ src/index.js
@@ -8,16 +8,13 @@ import '../node_modules/react-vis/dist/style.css';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
-import { BrowserRouter as Router } from 'react-router-dom';
 import thunk from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
     <App />
-    </Router>
   </Provider>,
   document.getElementById("root")
 );
